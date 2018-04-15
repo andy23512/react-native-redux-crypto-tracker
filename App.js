fb04424484924606ca9a3535d2f48bd562bf2a53
andy23512 <andy23512@gmail.com>
2018-04-15 16:35:03 +0800

    add style in header

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Header } from './src/components';
 
 export default class App extends Component {
